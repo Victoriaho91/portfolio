b9ae174e36bc441ed932b1248644115be69ca7cb
Victoria Ho <victoriaho91@gmail.com>
2021-04-30 16:40:45 -0400

    css fixes

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -4,47 +4,78 @@ import * as introStyles from '../styles/intro.module.css'
 import { AnchorLink } from "gatsby-plugin-anchor-links";
 import sal from 'sal.js'
 import '../../node_modules/sal.js/dist/sal.css'
+import DownArrow from '../images/arrow.svg'
+
 
 const strings = [
-  'Fullstack Engineer.',
+  '^1200 Web Developer.',
   'Creator.',
-  'Visual Thinker.',
-  'Daydreamer.'
+  'Fullstack Engineer.'
 ]
 
 
 export default function Intro (){
   sal({
     threshold:1,
-    once: false
+    once: true
   });
   return(
-    <div id="intro-container" className={introStyles.introContainer}>
-      <div className={introStyles.intro}>
-        <span className={introStyles.introText}
-        data-sal-duration={"2000"}
-        data-sal={"slide-up"}
-        data-sal-delay={"1200"}
-        data-sal-easing={"ease-out-bounce"}
-        >
-        Hello, I'm Victoria. <br/>
-        </span>
-        <span style={{marginTop:'-6vw' ,fontSize: '2vw',  display: 'inline-block',verticalAlign: 'middle'}}
-        data-sal-duration={"1200"}
-        data-sal={"fade"}
-        data-sal-delay={"1000"}
-        data-sal-easing={"ease"}
-        >
-        <Typed strings={strings} typeSpeed={80} backSpeed={50} loop/>
 
-        </span>
-      <div className={introStyles.buttonContainer} >
-      <AnchorLink className={introStyles.button} to='/#about-container'  title='about'>
-      View More
-      {/* <button className={introStyles.button} >View More</button> */}
-      </AnchorLink>
+    <div id="intro-container" className={introStyles.introContainer}>
+
+      <div className={introStyles.intro}>
+        <div className={introStyles.introText}>
+          <span
+            // data-sal-duration="1000"
+            // data-sal="slide-left"
+            // data-sal-delay="500"
+            // data-sal-easing="ease-out-bounce"
+          >
+            Hey there, <br/>
+          </span>
+          <span
+            // data-sal-duration="1000"
+            // data-sal="slide-left"
+            // data-sal-delay="900"
+            // data-sal-easing="easeOutCubic"
+          >
+            I'm <span style={{color:'#634EFF'}} >Victoria</span>.<br/>
+          </span>
+
+          <span
+          // data-sal-duration="1000"
+          // data-sal="slide-left"
+          // data-sal-delay="1000"
+          // data-sal-easing="easeOutCubic"
+
+          >
+         <Typed strings={strings} typeSpeed={95} backSpeed={50} loop/>
+
+
+          </span>
+
+        </div>
+
       </div>
+
+      <div className={introStyles.buttonContainer}
+        // data-sal-duration={"1200"}
+        // data-sal={"fade"}
+        // data-sal-delay={"2500"}
+        // data-sal-easing={"ease"}
+        // data-sal-once
+        >
+            <AnchorLink className={introStyles.button} to='/#about-container'  title='about'>
+              View More
+            <div className={introStyles.downArrow}>
+                <img className={introStyles.arrrow} style={{width: '60px'}} src={DownArrow} alt="down arrow" />
+
+            </div>
+            </AnchorLink>
+
+
       </div>
     </div>
+
   )
 }
